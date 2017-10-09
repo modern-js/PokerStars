58e8ff6b71508fcb76cecb3b973b169c83613176
hrist0stoichev <hrist0.stoichev@mail.bg>
2017-10-09 04:13:31 +0300

    Test game with console visualization added

--- src/test.js
+++ src/test.js
@@ -1,7 +1,24 @@
 const engine = require('./engine');
 
-const alreadyGenerated = new Set();
+const alreadyGeneratedCards = new Set();
 
-for (let i = 0; i < 26; i += 1) {
-    console.log(engine.generateCards(2, alreadyGenerated));
-}
+const river = engine.generateCards(5, alreadyGeneratedCards);
+const hands = new Array(8).fill(0).map(() => engine.generateCards(2, alreadyGeneratedCards));
+
+const rankedHands = engine.rankHands(river, hands);
+const winners = engine.determineWinner(rankedHands);
+
+console.log(`River cards:\n${river.map(card => card.signature).join('   ')}\n`);
+
+console.log(`Hands:\n${hands.map(hand => `${hand[0].signature}   ${hand[1].signature}`).join('\n')}\n`);
+
+console.log('Ranked Hands:');
+rankedHands.forEach((value, key) => {
+    console.log(`Cards - (${key.map(card => card.signature).join('  -  ')})    Rank - ${value.rank}    Best hand - (${value.bestCombination.map(card => card.signature).join('  -  ')})`);
+});
+console.log();
+
+console.log('Winners:');
+winners.forEach((value, key) => {
+    console.log(`Cards - (${key.map(card => card.signature).join('  -  ')})    Rank - ${value.rank}    Best hand - (${value.bestCombination.map(card => card.signature).join('  -  ')})`);
+});
