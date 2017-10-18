3638ff61603401ee57d541800ac94b30543275d8
hrist0stoichev <hrist0.stoichev@mail.bg>
2017-10-19 02:40:30 +0300

    create table component implemented

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import CreateTable from './CreateTable';
 
 export default function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <CreateTable createTable={(name, password) => console.log(name, password)} />
         </div>);
 }
---
+++ src/client/components/CreateTable.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class CreateTable extends React.Component {
+    static get propTypes() {
+        return {
+            createTable: React.PropTypes.func.isRequired,
+        };
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            password: '',
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Create new table</h3>
+                <form>
+                    <label htmlFor="name">Table name </label>
+                    <input type="text" id="name" name="name" value={this.state.name} onChange={(e) => { this.setState({ name: e.target.value }); }} />
+                    <br />
+                    <label htmlFor="password"> Password </label>
+                    <input type="password" id="password" name="password" value={this.state.password} onChange={(e) => { this.setState({ password: e.target.value }); }} />
+                    <br />
+                    <input type="button" value="Create" onClick={() => { this.props.createTable(this.state.name, this.state.password); }} />
+                </form>
+            </div>);
+    }
+}
