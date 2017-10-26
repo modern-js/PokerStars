992bb12a0b2ceeb1ada2ed5318040a2425c22d42
hrist0stoichev <hrist0.stoichev@mail.bg>
2017-10-26 21:07:13 +0300

    Simple router implemented

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CreateTable from './CreateTable';
+import Tables from './Tables';
 
 export default function App() {
     return (
-        <div>
-            <CreateTable createTable={(name, password) => console.log(name, password)} />
-        </div>);
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Tables} />
+                <Route exact path="/:tableId" component={CreateTable} />
+                <Route render={() => <div>Page not found</div>} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
