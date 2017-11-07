ea145baa9d483f1ca0e7c9add407de6a93e03766
hrist0stoichev <hrist0.stoichev@mail.bg>
2017-11-07 22:46:43 +0200

    some styles to the table added

--- src/client/components/Table.jsx
+++ src/client/components/Table.jsx
@@ -14,6 +14,32 @@ export default class Table extends Component {
         }).isRequired,
     };
 
+    static tableStyles = {
+        width: '70%',
+        minWidth: '500px',
+        height: '75%',
+        minHeight: '250px',
+        backgroundColor: 'red',
+        position: 'absolute',
+        left: '15%',
+        top: '8%',
+        borderRadius: '30%/50%',
+        border: '15px double black',
+    };
+
+    static playerStyles = {
+        width: '16%',
+        height: '12%',
+        backgroundColor: 'green',
+        border: '2px solid black',
+        textAlign: 'center',
+        position: 'absolute',
+        overflow: 'hidden',
+        padding: '1%',
+        borderRadius: '50%',
+        lineHeight: '5vh',
+    };
+
     constructor(props) {
         super(props);
 
@@ -38,7 +64,16 @@ export default class Table extends Component {
 
         return (
             <div>
-                <div>{ table.name }</div>
+                <div style={Table.tableStyles}>
+                    <div style={{ ...Table.playerStyles, left: '5%', top: '-5%' }}>asd<br />chips</div>
+                    <div style={{ ...Table.playerStyles, left: '50%', top: '-12%', transform: 'translate(-50%, 0)' }}>asd<br />chips</div>
+                    <div style={{ ...Table.playerStyles, right: '5%', top: '-5%' }}>asd<br />chips</div>
+                    <div style={{ ...Table.playerStyles, right: '-12%', top: '50%', transform: 'translate(0, -50%)' }}>asd<br />chips</div>
+                    <div style={{ ...Table.playerStyles, left: '5%', bottom: '-5%' }}>asd<br />chips</div>
+                    <div style={{ ...Table.playerStyles, left: '50%', bottom: '-12%', transform: 'translate(-50%, 0)' }}>asd<br />chips</div>
+                    <div style={{ ...Table.playerStyles, right: '5%', bottom: '-5%' }}>asd<br />chips</div>
+                    <div style={{ ...Table.playerStyles, left: '-12%', top: '50%', transform: 'translate(0, -50%)' }}>asd<br />chips</div>
+                </div>
             </div>
         );
     }
