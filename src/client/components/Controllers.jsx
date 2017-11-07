af4916ac2c0ee0560b0e16937bfafce2fffda646
hrist0stoichev <hrist0.stoichev@mail.bg>
2017-11-07 23:34:32 +0200

    controllers component implemented

---
+++ src/client/components/Controllers.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Controllers extends Component {
+    static controllersStyles = {
+        bottom: '3%',
+        right: '3%',
+        position: 'absolute',
+        border: '2px solid black',
+    };
+
+    static step = 10;
+
+    static propTypes = {
+        maxBet: PropTypes.number.isRequired,
+        amountToCall: PropTypes.number.isRequired,
+        canCall: PropTypes.bool.isRequired,
+        canRaise: PropTypes.bool.isRequired,
+        handleAction: PropTypes.func.isRequired,
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            betValue: this.props.amountToCall + Controllers.step,
+        };
+    }
+
+    render() {
+        return (
+            <div style={Controllers.controllersStyles}>
+                <div>
+                    <input type="range" value={this.state.betValue} min={this.props.amountToCall + Controllers.step} max={this.props.maxBet} step={Controllers.step} onChange={e => this.setState({ betValue: e.target.value })} />
+                    <span>{ this.state.betValue }</span>
+                </div>
+                <div>
+                    <button onClick={this.props.handleAction(0)}>Fold</button>
+                    <button disabled={this.props.canCall} onClick={this.props.handleAction(1)}>Call{this.props.canCall ? ` ${this.props.amountToCall}` : ''}</button>
+                    <button disabled={this.props.canRaise} onClick={this.props.handleAction(2, this.state.betValue)}>Raise To {this.props.canRaise ? ` ${this.state.betValue}` : ''}</button>
+                </div>
+            </div>
+        );
+    }
+}
