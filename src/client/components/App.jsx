7b02d3c40f1b5979bb0372774c98667f6b425a08
hrist0stoichev <hrist0.stoichev@mail.bg>
2017-11-08 02:43:21 +0200

    Spot component implemented

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import Tables from './Tables';
+import Lobby from './Lobby';
 import Table from './Table';
 
 export default function App() {
     return (
         <HashRouter>
             <Switch>
-                <Route exact path="/" component={Tables} />
+                <Route exact path="/" component={Lobby} />
                 <Route exact path="/table/:id" component={Table} />
                 <Route render={() => <div>Page not found</div>} />
             </Switch>
